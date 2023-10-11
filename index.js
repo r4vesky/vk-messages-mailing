43f627b2db7f1154fd10e1faaafad7b23e829959
r4vesky <r4vesky@gmail.com>
2023-10-12 01:01:42 +0700

    Writted version 2 / Stable

--- index.js
+++ index.js
@@ -19,63 +19,68 @@ const keyboard = {};
   let { count } = await vk.api.messages.getConversations();
 
   let iterations = Math.round(count / 100);
-  let promises = [];
+  let ids = [];
 
-  let processedItems = 0;
-  let processedIds = [];
+  let startedAt = Date.now();
 
   console.log(`Iterations count: ${iterations}`);
 
   for (var index = 0; index < iterations; index++) {
-    promises.push(
-      new Promise(async (resolve, reject) => {
-        try {
-          let currentIterationConversations =
-            await vk.api.messages.getConversations({
-              count: 100,
-              offset: index * 100,
-            });
+    try {
+      let currentIterationConversations =
+        await vk.api.messages.getConversations({
+          count: 100,
+          offset: index * 100,
+        });
 
-          let { items } = currentIterationConversations;
+      let { items } = currentIterationConversations;
 
-          let user_ids = items
-            .filter((item) => {
-              let { conversation } = item;
+      let user_ids = items
+        .filter((item) => {
+          let { conversation } = item;
 
-              let isUser = conversation.peer.type === "user";
-              let canWrite = conversation.can_write.allowed === true;
+          let isUser = conversation.peer.type === "user";
+          let canWrite = conversation.can_write.allowed === true;
 
-              return isUser && canWrite;
-            })
-            .filter((item) => !processedIds.includes(item.conversation.peer.id))
-            .map((item) => item.conversation.peer.id);
+          return isUser && canWrite;
+        })
+        .map((item) => item.conversation.peer.id);
 
-          processedIds = processedIds.concat(user_ids);
-
-          processedItems += user_ids.length;
-
-          let params = {
-            user_ids,
-            message,
-            attachment,
-            random_id: Math.random(),
-          };
-
-          if (Object.keys(keyboard).length > 0) {
-            params["keyboard"] = JSON.stringify(keyboard);
-          }
-
-          await vk.api.messages.send(params);
-
-          resolve();
-        } catch (error) {
-          reject(error);
-        }
-      })
-    );
+      ids = ids.concat(user_ids);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
-  return Promise.all(promises)
-    .finally(() => console.log(`Messages sended for ${processedItems} users`))
-    .catch(console.error);
+  setTimeout(() => {
+    console.log("Starting mailing...");
+    console.log("------------------------------------------------");
+
+    Array(iterations)
+      .fill({})
+      .forEach(async (_, index) => {
+        let params = {
+          user_ids: ids.slice(index * 100, index * 100 + 99),
+          message,
+          attachment,
+          random_id: Math.random(),
+        };
+
+        if (Object.keys(keyboard).length > 0) {
+          params["keyboard"] = JSON.stringify(keyboard);
+        }
+
+        await vk.api.messages.send(params);
+
+        console.log(
+          `Iteration #${index + 1} | Users: ${params.user_ids.length}`
+        );
+
+        if (index * 100 + 99 >= ids.length) {
+          console.log(`------------------------------------------------`);
+          console.log(`Completed in ${(Date.now() - startedAt) / 1000}s`);
+          console.log(`Totally sended: ${ids.length} messages`);
+        }
+      });
+  }, 1);
 })();
